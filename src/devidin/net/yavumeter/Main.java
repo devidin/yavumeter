a16d46a91465ece9fb0b9f8c7cfe8b0a1f032496
devidin <57951023+devidin@users.noreply.github.com>
2024-02-09 10:36:15 +0100

    first cmmit

---
+++ src/devidin/net/yavumeter/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package devidin.net.yavumeter;
+
+/**
+ * 
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/devidin/net/yavumeter/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+package devidin.net.yavumeter;
