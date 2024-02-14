17f7381ba1e8026ed77547f56089749a445da840
devidin <57951023+devidin@users.noreply.github.com>
2024-02-14 17:04:19 +0100

    move logs

---
+++ src/main/java/devidin/net/yavumeter/display/graphical/GraphicalDisplayerParameters.java
@@ -0,0 +1,9 @@
+package devidin.net.yavumeter.display.graphical;
+
+public class GraphicalDisplayerParameters extends GraphicalDisplayerConfiguration {
+
+	public GraphicalDisplayerParameters() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
