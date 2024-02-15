8331e8d778071cacd983be260d1328fd57bd4d75
devidin <57951023+devidin@users.noreply.github.com>
2024-02-15 18:54:21 +0100

    missing new files

---
+++ src/main/java/devidin/net/yavumeter/display/graphical/GraphicalDisplayerController.java
@@ -0,0 +1,17 @@
+package devidin.net.yavumeter.display.graphical;
+import javafx.fxml.*;
+import javafx.scene.*;
+import javafx.scene.control.Button;
+import javafx.stage.*;
+import javafx.event.*;
+
+public class GraphicalDisplayerController {
+
+	public GraphicalDisplayerController() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@FXML
+	private Button button;
+	
+}
